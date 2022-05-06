8682aef611040dae332eb0aa2fcf223136e3ae71
ABBlogg <33747960+ABBlogg@users.noreply.github.com>
2022-05-06 14:25:49 +0800

    Update rake-c.c
    Factored out all code into parse.c and exec.c (currently exec.c just tests the rakefile data by printing to stdout)
    Can specify the name of the rakefile with commandline arguments, defaults to "Rakefile"

--- rake-c/rake-c.c
+++ rake-c/rake-c.c
@@ -2,31 +2,23 @@
 #include <stdio.h>
 #include "rakefile.h"
 #include "parse.h"
+#include "exec.h"
 
-FILE* STREAM; 			// A universally accessible filestream to read the Rakefile
-char BUFFER[MAX_LINE_LEN];	// A buffer to store each line of the Rakefile (one at a time)
-
-int main() {
-
-	STREAM = fopen("Rakefile", "r");
-
-	/*Rakefile* rf = parseRakefile();
-
-	printf("Listening on port %i\n", rf->port);
-	for(int i = 0; i < rf->hCount; i++) {
-		printf("Host #%i is called %s\n", i, rf->hosts[i]);
-	}
-
-	for(int i = 0; i < rf->actionsets[0]->aCount; i++) {
-		printf("Actionset 1's Action #%i is called %s\n", i, rf->actionsets[0]->actions[i]->command);
-	}
-	*/
-
-	for(int i = 0; i < 100; i++) {
-		read_line(STREAM, BUFFER);
-	}
-
-	fclose(STREAM);
+int main(int argc, char** argv) {
+	
+	Rakefile* rf;
+	
+	
+	printf("Parsing Rakefile:\n");
+	
+	if(argc == 2) rf = parseRakefile(argv[2]);
+	else rf = parseRakefile("Rakefile");
+	
+	
+	
+	printf("\n\nExecuting Rakefile:\n");
+	
+	execRakefile(rf);
 
 	return 0;
 }
